dd2dfc88eab3f5165e7a569317f57f9566a5405f
Juup Pikkemaat <Pikkemaatjuup@gmail.com>
2023-03-27 12:15:57 +0200

    rework of files, added docker script, deployment script

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:latest
+
+ENV TZ=Europe/Amsterdam
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+WORKDIR /webapp
+
+COPY /webapp /
+EXPOSE 3000
+CMD ["node", "/build/index.js"]
---
+++ deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+SERVER_IP="130.162.38.88"
+KEY_PATH="/Users/juuppikkemaat/oracle.pub"
+# Usage:
+#  build - Create/Update docker container for the backend application
+#  run   - Run the backend as a container detached.
+#  stop  - Stop the backend container and remove it.
+
+if [ $# == 0 ]
+then
+    echo "Please supply a command."
+    echo "Commands: deploy"
+else
+    if [ $1 == "deploy" ]
+    then
+        echo "Stopping the backend containers."
+        ssh -i $KEY_PATH ubuntu@$SERVER_IP 'CONTAINERS=$(sudo docker ps -a -q --format="{{.ID}}") && sudo docker stop $CONTAINERS'
+        echo "Removing the backend containers"
+        ssh -i $KEY_PATH ubuntu@$SERVER_IP 'CONTAINERS=$(sudo docker ps -a -q --format="{{.ID}}") && sudo docker rm $CONTAINERS'
+
+        echo "Starting the container on server."
+        ssh -i $KEY_PATH ubuntu@$SERVER_IP "cd testregex && git pull && sudo docker build -t testregex ."
+        ssh -i $KEY_PATH ubuntu@$SERVER_IP "sudo docker run -d -p 80:3000 testregex"
+    fi
+fi
