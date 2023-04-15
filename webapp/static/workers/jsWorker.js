6fdfd8b0e1a1b5643659894d1de75f5dc430740b
Juup Pikkemaat <Pikkemaatjuup@gmail.com>
2023-04-16 00:52:02 +0200

    javascript engine implemented

--- webapp/static/workers/jsWorker.js
+++ webapp/static/workers/jsWorker.js
@@ -1,38 +1,55 @@
-function jsMatch(data) {
-    for (var expression_results, 
-        expression = new RegExp(data.regex, data.options), 
-        result = [], 
-        group_number = 0,
-        match_number = 0;
-        expression_results = expression.exec(data.regexText);
-        ) {
-            expression_results.index == expression.lastIndex && expression.lastIndex++;
-            
-            for (var result_data = [], index = 0; index < expression_results.length; index++){
-                result_data[index]  = {
-                    content: expression_results[index],
-                    start: expression_results.index+index,
-                    end: index > 0 ? (expression_results.index + expression_results[index].length)+1 : (expression_results.index + expression_results[index].length),
-                };
-                
-                // found expressions is greater than one meaning there are groups.
-                if (index > 0){
-                    result_data[index].name = Object.keys(expression_results.groups)[index-1];
-                    group_number += 1;
-                    result_data[index].group_number = group_number;
-                }
+function executeExpression(regex, flags, test_string) {
+    !flags.includes("d") && (flags += "d");
+
+    let expression = new RegExp(regex,flags)
+        , result = []
+        , global = flags.includes("g");
+    let match;
+    while ((match = expression.exec(test_string)) !== null) {
+        match.index === expression.lastIndex && expression.lastIndex++;
+        let sub_result = [];
+        for (let index = 0; index < match.length; index++) {
+            let start_index, end_index, groupName;
+            // Check if there are groups and group names.
+            if (match.groups && Object.keys(match.groups).length > 0) {
+                groupName = Object.keys(match.groups).find(key => match.groups[key] === match[index]);
+            }
+            // Make sure there are match indices
+            null != match.indices && null != match.indices[index] ? (start_index = match.indices[index][0],
+            end_index = match.indices[index][1]) : 0 === index && (start_index = match.index,
+            end_index = match.index + match[0].length);
+            let match_data = {
+                content: match[index],
+                group_number: index,
+                name: groupName,
+                start: start_index,
+                end: end_index
+            };
+            sub_result.push(match_data)
         }
-        
-        result[match_number++] = result_data; 
+        result.push(sub_result)
+        if (!global) break; 
     }
-    return { catastrophic: false, highlighter: result }
+    return {
+        highlighter: result
+    }
+}
+
+function jsMatch(test_data) {
+    try {
+        let start_time = performance.now();
+        let result = executeExpression(test_data.data.regex, test_data.data.options, test_data.data.regexText);
+        result.time = performance.now() - start_time;
+        return result;
+    } catch (e) {
+        return{error: e.message}
+    }
 }
 
 var debug = true;
-
-self.onmessage = function (test_data) {
-    self.postMessage("onload");
-    let result = jsMatch(test_data.data);
+self.onmessage = function(test_data) {
+    self.postMessage("onload")
+    let result = jsMatch(test_data);
     debug && console.log(result, "result")
-    self.postMessage(result)
-};
+    self.postMessage(result);
+}
