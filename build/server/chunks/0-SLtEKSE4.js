b443aef30b00c989fdd08510247c2975ac201731
Juup <80424975+J-Nova@users.noreply.github.com>
2024-01-09 15:53:14 +0100

    Development (#49)
    * Removed unneeded data
    
    * Added build

---
+++ build/server/chunks/0-SLtEKSE4.js
@@ -0,0 +1,9 @@
+const index = 0;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_layout.svelte-X7nM15u8.js')).default;
+const imports = ["_app/immutable/nodes/0.mnGdQk7I.js","_app/immutable/chunks/scheduler.ODeSHzXk.js","_app/immutable/chunks/index.ssg_r2_O.js","_app/immutable/chunks/SvelteToast.svelte_svelte_type_style_lang.JZuWg6pz.js","_app/immutable/chunks/index.ID0k71k8.js"];
+const stylesheets = ["_app/immutable/assets/0.7p9CuPAU.css","_app/immutable/assets/SvelteToast.2y77VXxx.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=0-SLtEKSE4.js.map
